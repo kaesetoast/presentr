85ac37f71ab70cf44857a7d6f932d369c6ab3635
Philipp Nowinski <philipp@nowinski.de>
2014-04-06 19:07:26 +0200

    connect speakerview to session

--- client/speakerview.js
+++ client/speakerview.js
@@ -1,3 +1,13 @@
-var draggable = require('draggable');
+var draggable = require('draggable'),
+    currentView = window.frames['current-view'],
+    nextView = window.frames['next-view'];
 draggable(document.getElementById('current-view'));
 draggable(document.getElementById('next-view'));
+
+currentView.document.addEventListener('DOMContentLoaded', function() {
+    currentView.presentation.connect();
+});
+
+nextView.document.addEventListener('DOMContentLoaded', function() {
+    nextView.presentation.connect();
+});
