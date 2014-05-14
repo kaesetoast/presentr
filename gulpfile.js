f406cc5fa4b57901a9fe6fe50f989fd4dd8d01af
Philipp Nowinski <philipp@nowinski.de>
2014-05-14 14:22:44 +0200

    moved from grunt to gulp

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    jshint = require('gulp-jshint'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    notify = require('gulp-notify'),
+    expressService = require('gulp-express-service'),
+    livereload = require('gulp-livereload');
+
+gulp.task('js-frontend', function(){
+    gulp.src('./client/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+
+    browserify('./client/app.js')
+    .bundle({debug: true})
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('public'));
+
+    browserify('./client/speakerview.js')
+    .bundle({debug: true})
+    .pipe(source('speakerview.js'))
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('js-backend', function(){
+    gulp.src(['./core/**/*.js', 'app.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(expressService({file:'./app.js', NODE_ENV:'DEV'}))
+    .pipe(notify({message: 'Reload App'}));
+});
+
+gulp.task('css', function(){
+    return gulp.src('./core/styles/*.scss')
+    .pipe(sass({style: 'expanded'}))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('public/'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('./core/styles/*.scss', ['css']);
+    gulp.watch('./client/**/*.js', ['js-frontend']);
+    gulp.watch(['./core/**/*.js', 'app.js'], ['js-backend']);
+    var server = livereload();
+    gulp.watch(['./public/**/*', './core/**/*.js']).on('change', function(file){
+        if (file.path.indexOf('/core/') > 0) {
+            setTimeout(function(){
+                server.changed(file.path);
+            }, 300);
+        } else {
+            server.changed(file.path);
+        }
+    });
+});
+
+gulp.task('default', function(){
+    gulp.start('css', 'js-frontend', 'js-backend', 'watch');
+});
